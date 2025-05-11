a2b09cde01f2d3ab4a1602219ffbabfb86eeca4e
dillipacharya19 <dillipacharya12345@gmail.com>
2025-05-11 22:10:37 +0530

    I have setup some basic setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='bg-amber-700 text-white text-center'>Vite + React</h1>
+    
+      
+    </>
+  )
+}
+
+export default App
